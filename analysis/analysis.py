0413b47fa874fbf0c2b2b11bfe1128968466f2e9
mm7490 <muellnermarco@gmail.com>
2018-03-14 19:47:09 +0100

    - Added removed resultslist again

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -44,6 +44,8 @@ def run(file: str, snrCriterion: float, windowSize: float, **kwargs: dict):
             warnings.simplefilter("ignore")
             frequencyList = recursiveFrequencyFinder(data,snrCriterion,windowSize
                                                      ,frequencyRange=kwargs['frequencyRange'],mode=kwargs['outputMode'])
+            result[(data[0][0], max(data[0]))] = frequencyList
+
 
 
     waitForProcessesFinished()
