b44843b21553b297bebdcbed49041049d7035571
mm7490 <muellnermarco@gmail.com>
2018-07-20 17:29:03 +0200

    - Fixed install process with pip>=10.0.0

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 import pip
+ver = pip.__version__.split(".")
+
+#main is not directly accessible anymore on pip > 10
+if int(ver[0]) < 10:
+    from pip import main
+else:
+    from pip._internal import main
+
 from setuptools import setup,find_packages
 
 reqs = ["pandas==0.22.0"
@@ -13,11 +21,11 @@ reqs = ["pandas==0.22.0"
 
 #this is a bit of a hack, due to the inability to build numpy properly in a reasonable amount of time
 for req in reqs:
-    pip.main(["install", req])
+        main(["install", req])
 
 setup(
     name='smurfs',
-    version='0.3.0',
+    version='0.3.1',
     packages=find_packages(exclude=["*test","dist","build","venv"]),
     url='https://github.com/muma7490/SMURFS',
     license='MIT',
