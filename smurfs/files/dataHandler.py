Fixed a bug in phase calculation

smurfs/files/dataHandler.py
@@ -14,7 +14,7 @@ def normalizeData(data: np.ndarray) -> np.ndarray:
     :param data: The dataset, probably read through readData
     :return: A normalized dataset
     """
-    data[0] -= data[0][0]
+#    data[0] -= data[0][0]
 
     data = reduce_y(data)
     return data
@@ -97,14 +97,17 @@ def getSplits(data: np.ndarray, timeRange: float = -1, overlap: float = 0,ignore
     :return: A list of chunks of the original data
     """
 
+    if timeRange == -1:
+        return [data]
+
     arg = np.argsort(data[0])
     data = np.array((data[0][arg],data[1][arg]))
 
     if data[0][0] != 0:
         data = normalizeData(data)
 
-    if timeRange == -1:
-        timeRange = max(data[0])
+    #if timeRange == -1:
+    #    timeRange = max(data[0])
     dataPoints = []
     i = 0
 

smurfs/support/plot.py
@@ -16,8 +16,7 @@ def plot_timeseries(title : str, data : np.ndarray):
     fig : Figure = pl.figure(figsize=(12,4))
     ax : Axes = fig.add_subplot(111)
 
-    data[0] += conf().zero_time
-    ax.plot(data[0],data[1],'x',markersize=3,color='k')
+    ax.plot(data[0] + conf().zero_time,data[1],'x',markersize=3,color='k')
     ax.set_xlabel("Time (days)")
     ax.set_ylabel("Flux")
     ax.set_title(title)

smurfs/timeseries/timerseries.py
@@ -122,13 +122,14 @@ def findAndRemoveMaxFrequency(lightCurve: np.ndarray, ampSpectrum: np.ndarray) -
     """
     maxY, maxX = findMaxPowerFrequency(ampSpectrum)
     popt = [-1, -1, -1]
-    arr = [maxY, maxX, 0]
+    arr = [maxY, maxX, np.pi/2]
+    limits = [[0, 0, 0], [np.inf, np.inf, np.inf]]
 
     # First fit could provide negative values for amplitude and frequency if the phase is moved by pi. Therefore run
     # again, with phase moved by pi as long as we don't have positive values for frequency and amplitude
     while popt[0] < 0 or popt[1] < 0:
         try:
-            popt, pcov = curve_fit(sin, lightCurve[0], lightCurve[1], p0=arr)
+            popt, pcov = curve_fit(sin, lightCurve[0], lightCurve[1], p0=arr,bounds=limits)
         except RuntimeError:
             print(term.format("Failed to find a good fit for Frequency " + str(maxX) + "c/d", term.Color.RED))
             raise RuntimeError